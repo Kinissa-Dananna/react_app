24bb23df61da74c7f50800644f3749b19aad9701
Anna R Pack <annarpack@gmail.com>
2017-10-26 12:24:48 -0400

    added event list and event tile

--- bar-crawl-react/src/components/bar-components/EventCard.js
+++ bar-crawl-react/src/components/bar-components/EventCard.js
@@ -4,16 +4,14 @@ import { BrowserRouter, Route, Link, Redirect } from "react-router-dom";
 class EventCard extends Component {
 	constructor(props){
 		super(props);
-		this.state ={}
 	}
 
-	// function that formats the information for each event 
 
 	render(){
 
 		return(
 			<div className="event-card">
-						
+				{this.props.eventsItems}
 			</div>
 		);
 	}
