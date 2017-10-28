0380f21e1cedf74e1c6f50896372d06364f6352c
Kin L Lim <Lim.Kin@gmail.com>
2017-10-28 10:25:22 -0400

    Delete Redirect Resolved

--- bar-crawl-react/src/components/bar-components/SingleEventView.js
+++ bar-crawl-react/src/components/bar-components/SingleEventView.js
@@ -1,51 +1,80 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 // import EventsBar from './EventsBar.js';
 import axios from 'axios';
 
 class SingleEventView extends Component {
 	constructor(props) {
-    super(props);
-    this.state = {
-      event: [],
-      bars:[],
-      attendees:[],
-    }
-  }
+		super(props);
+		this.state = {
+			event: [],
+			bars: [],
+			attendees: [],
+			deleted: false
+		}
+		this.deleteEvent = this.deleteEvent.bind(this);
+		this.redirectPage = this.redirectPage.bind(this);
+	}
 
 	// function that gets information from the database for a single event
-  	componentDidMount() {
-			//console.log(this.props.match);
-	    const eventId = this.props.match.params.id;
-			//console.log(eventId);
-		  	axios
-			  	.get(`http://localhost:8080/events/${eventId}?auth_token=${this.props.user.token}`)
-			    .then(response => {
-						//console.log(response.data.bars)
-		        	this.setState({
-								event: response.data,
-								bars: response.data.bars,
-								attendees: response.data.attendees
-							})
-		        });
+	componentDidMount() {
+		//console.log(this.props.match);
+		const eventId = this.props.match.params.id;
+		//console.log(eventId);
+		axios
+			.get(`http://localhost:8080/events/${eventId}?auth_token=${this.props.user.token}`)
+			.then(response => {
+				//console.log(response.data.bars)
+				this.setState({
+					event: response.data,
+					bars: response.data.bars,
+					attendees: response.data.attendees
+				})
+			});
+	}
+	redirectPage() {
+		const id = this.state.event.id;
+		if (this.state.deleted) {
+			console.log('this.state.submitted = true');
+			return <Redirect from={`/events/${id}`} exact to={`/events/${id}`} />
 		}
+	}
+
+	deleteEvent() {
+		console.log('delete click')
+		//const eventId = Number(this.props.match.params.id);
+		const id = this.state.event.id;
+		console.log('id', id);
+		axios.delete(`http://localhost:8080/events/${id}?auth_token=${this.props.user.token}`,
+			{ id })
+			.then(res => {
+				console.log(res);
+				this.setState({ deleted: true });
+				this.props.history.push(`/events`);
+			})
+	}
 
 
 	// Formatted information for a single event
-	render(){
+	render() {
 		const { name, description, time } = this.state.event;
 		const eventId = this.props.match.params.id;
 		const bars = this.state.bars.map((bar) => {
 			//console.log(bar.name);
-				return (
-					<Link to={`/events/${eventId}/bars/${bar.id}`} >{bar.name}<br /></Link>
-				)
-			})
+			return (
+				<Link to={`/events/${eventId}/bars/${bar.id}`} >{bar.name}<br /></Link>
+			)
+		})
 		const attendees = this.state.attendees.map((user, key) => {
 			console.log(user.name);
-				return <p id={key} >{user.name}</p>
-			})
-		return(
+			return <p id={key} >{user.name}</p>
+		})
+
+		if (this.state.deleted) {
+			return <Redirect to={'/events'} />
+		}
+
+		return (
 			<div className="single-event">
 				<div className="event-info">
 					<h2>{name}</h2>
@@ -62,6 +91,8 @@ class SingleEventView extends Component {
 					<h4>Attending:</h4>
 					<p>{attendees}</p>
 				</div>
+
+				<button onClick={() => this.deleteEvent(eventId)} > Delete This Event </button>
 			</div>
 		);
 	}
